a5a6ec182a840986ea8376e295e465b58cc5edff
imadcollin <imad.collin@gmail.com>
2021-02-05 12:55:30 +0100

    First task solution

--- Start.py
+++ Start.py
@@ -1,6 +1,25 @@
 #%%
-import pandas as pd 
-print ("Hello World")
-df = pd.read_csv('sample.csv'); 
-print (df.head(10))
-# %%
+import pandas as pd
+df = pd.read_csv('sample.csv')
+print(df.head(10))
+
+
+# %% Cleanup salaries, remove $
+def clean_salary(row):
+    salary = row['Salary'].replace('$', '')
+    return float(salary)
+
+
+# %% Add a new row for cleaned salaries
+df['cleaned_salary'] = df.apply(clean_salary, axis=1)
+
+# %% Print
+print(df.head(10))
+
+#%% Salaries mean
+salaries_mean = df['cleaned_salary'].mean()
+print(salaries_mean)
+
+# %% Salaries mean Grouped By Gender
+salaries_by_gender = df.groupby('gender')['cleaned_salary'].mean()
+print(salaries_by_gender)
